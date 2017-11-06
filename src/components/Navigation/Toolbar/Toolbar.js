bfae086d93c63b890ac5b0a01f82094c61a08462
Farid Ansari <farid12ansari7@gmail.com>
2017-11-06 06:38:35 +0530

    toolbar placement

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from "./Toolbar.css"
+
+const toolBar = props => (
+  <header className={classes.Toolbar}>
+    <div>Menu</div>
+    <div>Logo</div>
+    <nav>
+      <ul>...</ul>
+    </nav>
+  </header>
+);
+
+export default toolBar;

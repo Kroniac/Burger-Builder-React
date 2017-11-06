cd313841851dea1ba6adccaa00d7d86c209f9436
Farid Ansari <farid12ansari7@gmail.com>
2017-11-06 06:56:36 +0530

    Burger logo added

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import classes from "./Toolbar.css"
+import Logo from "../../UI/Logo/Logo"
 
 const toolBar = props => (
   <header className={classes.Toolbar}>
     <div>Menu</div>
-    <div>Logo</div>
+    <Logo />
     <nav>
       <ul>...</ul>
     </nav>

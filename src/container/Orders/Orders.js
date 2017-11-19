1a5dc6b2ef0def635d835d34f7f8fe154943ac18
Farid Ansari <farid12ansari7@gmail.com>
2017-11-19 09:52:34 +0530

    orders page maintained by redux

--- src/container/Orders/Orders.js
+++ src/container/Orders/Orders.js
@@ -2,40 +2,42 @@ import React, { Component } from "react";
 import Order from "../../components/Order/Order";
 import axios from "../../axios-order";
 import errorHandler from "../../hoc/withErrorHandler/withErrorHandler";
+import { connect } from "react-redux";
+import * as actions from "../../store/actions/index";
+import Spinner from "../../components/UI/Spinner/Spinner";
 class Orders extends Component {
-  state = {
-    order: [],
-    loading: true
-  };
-
   componentDidMount() {
-    axios
-      .get("/orders.json")
-      .then(res => {
-        let fetchedData = [];
-        for (let key in res.data)
-          fetchedData.push({
-            ...res.data[key],
-            id: key
-          });
-        this.setState({ loading: false, order: fetchedData });
-      })
-      .catch(err => {
-        this.setState({ loading: false });
-      });
+    this.props.onFetchOrders();
   }
   render() {
-    return (
-      
-      <div>
-       { this.state.order.map(order => (
-         <Order key={order.id}
-         ingredients = {order.ingredients}
-         totalPrice = {+order.price} />
-       ))}
-      </div>
+    let orders = this.props.loading ? (
+      <Spinner />
+    ) : (
+      this.props.orders.map(order => (
+        <Order
+          key={order.id}
+          ingredients={order.ingredients}
+          totalPrice={+order.price}
+        />
+      ))
     );
+
+    return <div> {orders} </div>;
   }
 }
 
-export default errorHandler(Orders, axios);
+export const mapStateToProps = state => {
+  return {
+    orders: state.order.orders,
+    loading: state.order.loading
+  };
+};
+
+export const mapDispatchToProps = dispatch => {
+  return {
+    onFetchOrders: () => dispatch(actions.fetchOrders())
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(
+  errorHandler(Orders, axios)
+);

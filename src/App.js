c78056e8dea8b232a65392eabec0bc644389daa5
Farid Ansari <farid12ansari7@gmail.com>
2017-11-21 10:43:31 +0530

    logout added after signin and logout fuctionality added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React, { Component } from "react";
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./container/BurgerBuilder/BurgerBuilder";
 import Checkout from "./container/CheckOut/CheckOut";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Orders from "./container/Orders/Orders";
 import Auth from "./container/Auth/Auth";
+import Logout from "./container/Auth/Logout/Logout";
 class App extends Component {
   render() {
     return (
@@ -14,6 +15,7 @@ class App extends Component {
             <Route path="/checkout" component={Checkout} />
             <Route path="/orders" component={Orders} />
             <Route path="/auth" component={Auth} />
+            <Route path="/logout" component={Logout} />
             <Route path="/" component={BurgerBuilder} />
           </Switch>
         </Layout>
